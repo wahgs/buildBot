e24dfbfc7576ce0133c303e32e95c33361d3966a
kyle <69319142+wahgs@users.noreply.github.com>
2023-03-18 09:56:13 -0400

    init progress II (forgot to stage lol)

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-import dotenv from 'dotenv'
-dotenv.config()
+//Necessary Discord.js classes
+const {Client, Events, GatewayIntenBits, ActivityFlags,  } = require('discord.js');
+const token = process.env.DISCORD_TOKEN
 
-import { Client, GatewayIntentBits } from 'discord.js'
+import {makeCategoryAndMessage, checkCategoryExists} from 'acts/utils.js'
 
-//creates a client
-const client = new Client({
-    intents: [
-        //intents loads classes for specific actions
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.DirectMessages,
-    ],
+//new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+//When the client is ready, run this code (once)
+// 'c' is the event parameter, kept seperate from the defined client
+client.once(Events.ClientReady, c => {
+    console.log(`Ready! logged in as ${c.user.tag}`) 
 });
 
-client.login(process.env.DISCORD_TOKEN);
+//Logs into Discord with the client's token
+client.login(token);
+
+//checks that there is no category already created
+if (!checkCategoryExists('buildBot')) {
+    console.log('Category \'buildBot\' doesn\'t exist, creating.')
+    //creates a category called \'buildBot\'
+    //creates a text channel, sends prompt message, reacts to said message.
+    makeCategoryAndMessage()
+}
