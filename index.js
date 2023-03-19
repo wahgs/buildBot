6d94ea9119f8c4c3946b7c93821928c285664eba
antja03 <antja.03@gmail.com>
2023-03-19 02:40:36 -0400

    Fix initialization to be in ready event callback

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //Necessary Discord.js classes
-const {Client, Events, GatewayIntenBits, ActivityFlags,  } = require('discord.js');
+const { Client, Events, GatewayIntenBits, ActivityFlags, } = require('discord.js');
 const token = process.env.DISCORD_TOKEN
 
-import {makeCategoryAndMessage, checkCategoryExists} from 'acts/utils.js'
+import { makeCategoryAndMessage, checkCategoryExists } from 'acts/utils.js'
 
 //new client instance
 export const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -10,16 +10,16 @@ export const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 //When the client is ready, run this code (once)
 // 'c' is the event parameter, kept seperate from the defined client
 client.once(Events.ClientReady, c => {
-    console.log(`Ready! logged in as ${c.user.tag}`) 
+    console.log(`Ready! logged in as ${c.user.tag}`)
+
+    //checks that there is no category already created
+    if (!checkCategoryExists('buildBot')) {
+        console.log('Category \'buildBot\' doesn\'t exist, creating.')
+        //creates a category called \'buildBot\'
+        //creates a text channel, sends prompt message, reacts to said message.
+        makeCategoryAndMessage()
+    }
 });
 
 //Logs into Discord with the client's token
 client.login(token);
-
-//checks that there is no category already created
-if (!checkCategoryExists('buildBot')) {
-    console.log('Category \'buildBot\' doesn\'t exist, creating.')
-    //creates a category called \'buildBot\'
-    //creates a text channel, sends prompt message, reacts to said message.
-    makeCategoryAndMessage()
-}
