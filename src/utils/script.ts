f860083f942be83001124a52ffdf48c31e96036c
kyle <69319142+wahgs@users.noreply.github.com>
2023-03-25 00:16:08 -0400

    addded embedBuild feature for ease of prompt
    Spent a long time trying to make the text script work in a way that isn't efficient. Goal 1 is to get the bot working enough for a 'alpha' , then add qol features like button interaction, and opencv usage.
    
    waste of two days.. :-1:

--- src/utils/script.ts
+++ src/utils/script.ts
@@ -1,23 +1,66 @@
 import { ActionRowBuilder } from '@discordjs/builders'
 import { client } from '../index'
-import { ButtonBuilder, ButtonStyle } from 'discord.js'
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Client, EmbedBuilder, Events, GatewayIntentBits, GuildTextBasedChannel, IntentsBitField } from 'discord.js';
+import { collectMessage } from '../lib/collector';
 
 
-
-
-export const speakScript = async (user) => {
-    //creates a button
-    const row = new ActionRowBuilder()
+export const buttonBuild = async (customId: string, label: string, style: ButtonStyle) => {
+  const row = new ActionRowBuilder()
         .addComponents(
             new ButtonBuilder()
-                .setCustomId('startBuild')
-                .setLabel('Start build:crossed_swords:')
-                .setStyle(ButtonStyle.Primary),
+                .setCustomId(customId)
+                .setLabel(label)
+                .setStyle(style),
     );
-    
-    //sends the message to the user
-  //  messageLink.send( { content: 'Let\'s being your build!', components: [row] });
-    
+  return [row]; //converted to a list so it can be called in the 'reply' discord.js function
+}
+
+export const embedBuild = async (title, description) => {
+  const embed = new EmbedBuilder()
+    .setTitle(title)
+    .setDescription(description)
+
+  return embed;
+  }
+//easily builds an embed for readibility
+
+
+
+export const speakScript = async (interaction, userId) => {
+  //READ
+  //This is essentially a message script between the bot, where the bot intakes parameters from the user.
+  
+  //variables
+  const userIdentification = userId;
+  //
+
+
+
+
+
+    //creates a button
+  await interaction.interaction({
+    embeds: [
+      embedBuild('Gamemode Selection', 'Please select which Call of Duty Modern Warfare II gamemode this weapon build is for with the buttons below.')
+    ],
+    components: [
+      buttonBuild('comp', 'Competitive', ButtonStyle.Danger),
+      buttonBuild('cas', 'Casual', ButtonStyle.Success)
+    ],
+    ephemeral: true
+  });
+
+  let gamemodeType = await collect(interaction.channel as GuildTextBasedChannel, interaction.user.id);
+  
+  await reply.delete();
+
+  await interaction.editReply({
+      content: '',
+      ephemeral: true
+  })
+
+
+
     /**
      * TODO:
      * GO OVER TEXT SCRIPT
